f2b6d771c6aaefe6c242a59e2a886988a391695c
KalebNyquist <49201801+KalebNyquist@users.noreply.github.com>
2019-08-21 13:07:37 -0400

    added Kickstarter JSON to CSV/Pandas DF functionality

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,25 +11,26 @@ Each support function should have an informative name and return the partially c
 """
 import pandas as pd
 
-def support_function_one(example):
-    pass
+def float_pledges(dirty_data):
+    dirty_data['Pledged'] = dirty_data['Pledged'].astype('float')
+    return dirty_data
 
 def support_function_two(example):
-    pass
+    return example
 
 def support_function_three(example):
-    pass
+    return example 
 
-def full_clean():
+def full_clean(dirty_data):
     """
     This is the one function called that will run all the support functions.
     Assumption: Your data will be saved in a data folder and named "dirty_data.csv"
 
     :return: cleaned dataset to be passed to hypothesis testing and visualization modules.
     """
-    dirty_data = pd.read_csv("./data/dirty_data.csv")
-
-    cleaning_data1 = support_function_one(dirty_data)
+    #dirty_data = pd.read_csv("./data/dirty_data.csv")
+    
+    cleaning_data1 = float_pledges(dirty_data)
     cleaning_data2 = support_function_two(cleaning_data1)
     cleaned_data= support_function_three(cleaning_data2)
     cleaned_data.to_csv('./data/cleaned_for_testing.csv')
